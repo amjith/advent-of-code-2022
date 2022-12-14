9a0c269d4591bcb4559b8418c1749315634583f7
Amjith Ramanujam <amjith.r@gmail.com>
2022-12-13 19:11:05 -0800

    day13.

--- day11/day11.nim
+++ day11/day11.nim
@@ -1,58 +1,58 @@
 import std/[sequtils, algorithm]
 
-type opFunc = proc(x: int64): int64
-type testFunc = proc(x: int64): int
+type opFunc = proc(x: int): int
+type testFunc = proc(x: int): int
 
 type
   Monkey = object
-    itms: seq[int64]
+    itms: seq[int]
     operation: opFunc
     test: testFunc
-    inspectCount: int64
+    inspectCount: int
 
 
-#proc part1(data: string): int64 =
+#proc part1(data: string): int =
 #  var monkeys = @[
 #                  Monkey(
 #                    itms: @[97, 81, 57, 57, 91, 61],
 #                    operation: proc(x: int): int = (x * 7) div 3,
 #                    test: proc(x: int): int = (if (x mod 11 == 0): 5 else: 6),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[88, 62, 68, 90],
 #                    operation: proc(x: int): int = (x * 17) div 3,
 #                    test: proc(x: int): int = (if (x mod 19 == 0): 4 else: 2),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[74, 87],
 #                    operation: proc(x: int): int = (x + 2) div 3,
 #                    test: proc(x: int): int = (if (x mod 5 == 0): 7 else: 4),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[53, 81, 60, 87, 90, 99, 75],
 #                    operation: proc(x: int): int = (x + 1) div 3,
 #                    test: proc(x: int): int = (if (x mod 2 == 0): 2 else: 1),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[57],
 #                    operation: proc(x: int): int = (x + 6) div 3,
 #                    test: proc(x: int): int = (if (x mod 13 == 0): 7 else: 0),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[54, 84, 91, 55, 59, 72, 75, 70],
 #                    operation: proc(x: int): int = (x * x) div 3,
 #                    test: proc(x: int): int = (if (x mod 7 == 0): 6 else: 3),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[95, 79, 79, 68, 78],
 #                    operation: proc(x: int): int = (x + 3) div 3,
 #                    test: proc(x: int): int = (if (x mod 3 == 0): 1 else: 3),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #                  Monkey(
 #                    itms: @[61, 97, 67],
 #                    operation: proc(x: int): int = (x + 4) div 3,
 #                    test: proc(x: int): int = (if (x mod 17 == 0): 0 else: 5),
-#                    inspectCount: 0'i64),
+#                    inspectCount: 0),
 #  ]
 #  for round in 0..19:
 #    for monkey in mitems(monkeys):
@@ -66,50 +66,51 @@ type
 #  return inspections[0] * inspections[1]
 
 
-proc part2(data: string): int64 =
+proc part2(data: string): int =
+  let commonDivisor = 11 * 19 * 5 * 2 * 13 * 7 * 3 * 17;
   var monkeys = @[
                   Monkey(
-                    itms: @[97, 81, 57, 57, 91, 61].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x * 7),
-                    test: proc(x: int64): int = (if (x mod 11 == 0): 5 else: 6),
-                    inspectCount: 0'i64),
+                    itms: @[97, 81, 57, 57, 91, 61],
+                    operation: proc(x: int): int = (x * 7) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 11 == 0): 5 else: 6),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[88, 62, 68, 90].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x * 17),
-                    test: proc(x: int64): int = (if (x mod 19 == 0): 4 else: 2),
-                    inspectCount: 0'i64),
+                    itms: @[88, 62, 68, 90],
+                    operation: proc(x: int): int = (x * 17) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 19 == 0): 4 else: 2),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[74, 87].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x + 2),
-                    test: proc(x: int64): int = (if (x mod 5 == 0): 7 else: 4),
-                    inspectCount: 0'i64),
+                    itms: @[74, 87],
+                    operation: proc(x: int): int = (x + 2) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 5 == 0): 7 else: 4),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[53, 81, 60, 87, 90, 99, 75].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x + 1),
-                    test: proc(x: int64): int = (if (x mod 2 == 0): 2 else: 1),
-                    inspectCount: 0'i64),
+                    itms: @[53, 81, 60, 87, 90, 99, 75],
+                    operation: proc(x: int): int = (x + 1) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 2 == 0): 2 else: 1),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[57].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x + 6),
-                    test: proc(x: int64): int = (if (x mod 13 == 0): 7 else: 0),
-                    inspectCount: 0'i64),
+                    itms: @[57],
+                    operation: proc(x: int): int = (x + 6) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 13 == 0): 7 else: 0),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[54, 84, 91, 55, 59, 72, 75, 70].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x * x),
-                    test: proc(x: int64): int = (if (x mod 7 == 0): 6 else: 3),
-                    inspectCount: 0'i64),
+                    itms: @[54, 84, 91, 55, 59, 72, 75, 70],
+                    operation: proc(x: int): int = (x * x) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 7 == 0): 6 else: 3),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[95, 79, 79, 68, 78].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x + 3),
-                    test: proc(x: int64): int = (if (x mod 3 == 0): 1 else: 3),
-                    inspectCount: 0'i64),
+                    itms: @[95, 79, 79, 68, 78],
+                    operation: proc(x: int): int = (x + 3) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 3 == 0): 1 else: 3),
+                    inspectCount: 0),
                   Monkey(
-                    itms: @[61, 97, 67].mapIt(int64(it)),
-                    operation: proc(x: int64): int64 = (x + 4),
-                    test: proc(x: int64): int = (if (x mod 17 == 0): 0 else: 5),
-                    inspectCount: 0'i64),
+                    itms: @[61, 97, 67],
+                    operation: proc(x: int): int = (x + 4) mod commonDivisor,
+                    test: proc(x: int): int = (if (x mod 17 == 0): 0 else: 5),
+                    inspectCount: 0),
   ]
-  for round in 0..20:
+  for round in 0..10000:
     for monkey in mitems(monkeys):
       for itm in mitems(monkey.itms):
         let worry = monkey.operation(itm)
--- day13/day13.py
+++ day13/day13.py
@@ -1,51 +1,60 @@
+from functools import cmp_to_key
+
 
 def check_order(left, right):
-    if isinstance(left, int) and not isinstance(right, int):
+    if isinstance(left, int) and isinstance(right, int):
         if left == right:
             return None
         else:
-            return left < right
-    if not isinstance(left, list) and isinstance(right, list):
-        if len(right) == 0:
-            return False
-        return check_order(left, right[0])
-    if isinstance(left, list) and not isinstance(right, list):
-        if len(left) == 0:
-            return True
-        return check_order(left[0], right)
-    for i, _ in enumerate(left):
-        if len(right) <= i:  # Right ran out of things. Left is longer (wrong)
-            return False
-        if isinstance(right[i], list) or isinstance(left[i], list):   # If either one of them is a list, unwrap
-            answer = check_order(left[i], right[i])
-            if answer is None:
+            return -1 if left < right else 1
+    if isinstance(left, list) and isinstance(right, list):
+        ans = None
+        for l, r in zip(left, right):
+            ans = check_order(l, r)
+            if ans is None:
                 continue
             else:
-                return answer
-        elif left[i] > right[i]:
-            return False
-        elif left[i] < right[i]:
-            return True
-    if len(left) < len(right):
-        return True
-    return None
+                return ans
+        if len(left) == len(right):
+            return ans
+        else:
+            return -1 if len(left) < len(right) else 1
+    if isinstance(left, int) and isinstance(right, list):
+        return check_order([left], right)
+    if isinstance(left, list) and isinstance(right, int):
+        return check_order(left, [right])
+
+
+def part2(filename):
+    packets = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            packets.append(eval(line))
+    packets.append([[2]])
+    packets.append([[6]])
+    results = sorted(packets, key=cmp_to_key(check_order))
+    return (results.index([[2]]) + 1) * (results.index([[6]]) + 1)
 
 
 def part1(filename):
     pair_idx = 0
     result = []
     with open(filename) as f:
-        for line in  f:
+        for line in f:
             pair_idx += 1
             left = eval(line)
             right = eval(f.readline().strip())
             _ = f.readline()
-            print(pair_idx, "left:", left)
-            print(pair_idx, "right:", right)
-            if check_order(left, right):
+            # print(pair_idx, "left:", left)
+            # print(pair_idx, "right:", right)
+            if check_order(left, right) == -1:
                 result.append(pair_idx)
-    # print(result)
+    print(result)
     return sum(result)
 
 
 print(part1("input.txt"))
+print(part2("input.txt"))
